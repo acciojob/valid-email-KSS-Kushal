ab4077488499a663fa397967f17107477a63009c
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-03-08 23:45:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+	const re = /^[a-z0-9.]*@[a-z]*.[a-z]{2,10}$/;
+	if (str.length<3) {
+		return false;
+	}
+	return re.test(str);
 }
 
 // Do not change the code below.
